b4627bf732d5533f25debd003e55908e4021469e
linabilou <biloapauline01@gmail.com>
2023-05-16 18:20:37 +0100

    les etapes 1 et 2 de la compression fait avec ajustement de la premiere version- introduction de la structure enregistrement pour associer et manipuler directement un caractere et son nombre d'occurence

--- huffman_compression.c
+++ huffman_compression.c
@@ -5,19 +5,45 @@
 #define TAILLE_TABLEAU 95                                                                                                               
 #define PREMIER_CARACTERE 32    
 
-int alphabet[TAILLE_TABLEAU];
-
-typedef struct caractere_occurence{
+typedef struct caractereOccurence{
     char caractere;
     int occurence;
-}caractere_occurence;
+}caractereOccurence;
+
+caractereOccurence alphabet[TAILLE_TABLEAU]; //on definit un tableau alphabet dont une case contient un caractere et son nombre d'occurence
+
+/*
+La fonction initialisation permet d'initialiser le tableau 
+- Lors de l'initialisation, les caracteres de chaque case correspondent aux indices du tableau et les occurences sont mis a 0 pour tout le monde
+Tout ceci pour nous faciliter la manipulation. 
+*/
+void initialisation()
+{
+    for(int indice = 0; indice < TAILLE_TABLEAU; indice++)
+    {
+        alphabet[indice].caractere = indice;
+        alphabet[indice].occurence = 0;
+    }
+}
+
+/*
+La fonction qui compare deux éléments > va etre utilise dans qsort qui est une fonction deja definie en C et a une structure bien definie
+- Elle prend en parametre deux pointeurs sur des constantes de n'importe quel type d'ou le void; et renvoie un entier
+- Dans notre cas on fait en sorte qu'elle prenne des pointeurs sur le type qu'on a defini
+*/
+int fonctionDeComparaison (const void *parametre1, const void *parametre2) {
+
+    int nombre1 = ((caractereOccurence *)parametre1)->occurence;
+    int nombre2 = ((caractereOccurence *)parametre2)->occurence;
+
+    return (nombre1 - nombre2);
+}
 
 /* 
 1 - La fonction qui compte le nombre d'occurence d'un mot
-    :param -> un mot
-    :return -> un tableau (de type enregistrement) qui contient chaque caractere contenus dans le mot avec son nombre d'occurence chacun
+    :param -> le chemin vers le fichier (le fichier a compresser)
+    :return -> un tableau (de type enregistrement) qui contient chaque caractere contenus dans le fichier avec son nombre d'occurence chacun
 */
-
 void compterOccurenceMot(char *cheminVersFichier){
     char caractereLu;
     FILE *fichierACompressser;
@@ -30,16 +56,19 @@ void compterOccurenceMot(char *cheminVersFichier){
     {
         int indice = caractereLu - PREMIER_CARACTERE; //indice represente l'indice du caractere lu dans le tableau
         if(indice >= 0 && indice < TAILLE_TABLEAU)
-            alphabet[indice]++;
+            alphabet[indice].occurence++;
+            
     }
     fclose(fichierACompressser);
+
+    // La fonction de la bibliothèque C void qsort(void *base, size_t nitems, size_t size, int (*compar)(const void *, const void*)) trie un tableau.
+    qsort(alphabet, TAILLE_TABLEAU, sizeof(caractereOccurence), fonctionDeComparaison);
     
-    for(int i = 0; i < TAILLE_TABLEAU; i++)
-        if(alphabet[i] != 0)
-            printf("car == %c , nbocc == %d\n", i + PREMIER_CARACTERE, alphabet[i]);
+    for(int indice = 0; indice < TAILLE_TABLEAU; indice++)
+        if(alphabet[indice].occurence != 0)
+            printf("caractere == %c , nboccurence == %d\n", alphabet[indice].caractere + PREMIER_CARACTERE, alphabet[indice].occurence);
    
 }
-
 
 int main(int nbArgument, char *parametres[]){
     if(nbArgument != 2)
@@ -48,6 +77,7 @@ int main(int nbArgument, char *parametres[]){
         exit(1);
     }
 
+    initialisation();
     compterOccurenceMot(parametres[1]);
     
     return 0;
