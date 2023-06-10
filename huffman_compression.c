d6ee38c8d2a2cc922952deb68658416e466348ca
linabilou <biloapauline01@gmail.com>
2023-06-10 17:06:13 +0100

    aucun changement dans le principe - juste renommage des fonctions

--- huffman_compression.c
+++ huffman_compression.c
@@ -5,33 +5,19 @@
 #define TAILLE_TABLEAU 95                                                                                                               
 #define PREMIER_CARACTERE 32    
 
-#define FICHIEROCCURENCE "caractereOccurence.txt"
-
 typedef struct caractereOccurence{
     char caractere;
     int occurence;
     char code[TAILLE_TABLEAU];
 }caractereOccurence;
 
-/*
-- Le tableau alphabet contient les caracteres ascii 32-126 d'ou la taille 95 (127-32)
-- Il contient les caracteres ascii possibles et leur donne d'occurence
-*/
+/* Le tableau alphabet contient les caracteres ascii 32-126 d'ou la taille 95 (127-32) - les caracteres ascii possibles et leur donne d'occurence */
 caractereOccurence alphabet[TAILLE_TABLEAU]; 
 
-
-/*
-- Le tableau noeudParent contient les racines obtenus a partir de la somme des occurences selon le principe de huffman
-*/
+/* Le tableau noeudParent contient les racines obtenus a partir de la somme des occurences selon le principe de huffman */
 int noeudParent[TAILLE_TABLEAU]; 
 
-
-
-/*
-cette fonction permet d'initialiser le tableau de l'alphabet
-- Lors de l'initialisation, les caracteres de chaque case correspondent aux indices du tableau et les occurences sont mis a 0 pour tout le monde
-Tout ceci pour nous faciliter la manipulation. 
-*/
+/*Initialisation de notre alphabet de caractere */
 void initialisationTableauAlphabet(){
     for(int indice = 0; indice < TAILLE_TABLEAU; indice++)
     {
@@ -41,12 +27,7 @@ void initialisationTableauAlphabet(){
     }
 }
 
-
-/*
-Cette fonction  compare deux éléments > Elle va etre utilise dans qsort qui est une fonction deja definie en C et a une structure bien definie
-- Elle prend en parametre deux pointeurs sur des constantes de n'importe quel type d'ou le void; et renvoie un entier
-- Dans notre cas on fait en sorte qu'elle prenne des pointeurs sur le type qu'on a defini
-*/
+/* Cette fonction  compare deux éléments */
 int fonctionDeComparaison (const void *parametre1, const void *parametre2){
 
     int nombre1 = ((caractereOccurence *)parametre1)->occurence;
@@ -55,14 +36,10 @@ int fonctionDeComparaison (const void *parametre1, const void *parametre2){
     return (nombre1 - nombre2);
 }
 
-/* 
-1 & 2 - Cette fonction  compte le nombre d'occurence des caracteres de notre fichier
-    :param -> le chemin vers le fichier (le fichier a compresser)
-    :return -> un tableau (de type enregistrement) qui contient chaque caractere contenus dans le fichier avec son nombre d'occurence chacun
-*/
-void compterOccurenceMot(char *cheminVersFichier){
+/*Procedure pour compter le nombre d'occurence et trie le tableau */
+void compterOccurence(char *cheminVersFichier){
     char caractereLu;
-    FILE *fichierACompressser, *fichierDesOccurences;
+    FILE *fichierACompressser;
     if ((fichierACompressser = fopen(cheminVersFichier,"r")) == NULL){
       fprintf(stderr, "\nErreur: Impossible de lire le fichier %s\n",cheminVersFichier);
       exit(1);
@@ -75,26 +52,13 @@ void compterOccurenceMot(char *cheminVersFichier){
     }
     fclose(fichierACompressser);
 
-    // La fonction de la bibliothèque C void qsort(void *base, size_t nitems, size_t size, int (*compar)(const void *, const void*)) trie un tableau.
-    /*Elle va donc nous permettre de trier notre tableau de caractere dans l'ordre croissant du nombre d'occurence*/
-    qsort(alphabet, TAILLE_TABLEAU, sizeof(caractereOccurence), fonctionDeComparaison);
-
-  
-    // for(int indice = 0; indice < TAILLE_TABLEAU; indice++)
-    //    if(alphabet[indice].occurence != 0)
-    //        printf("caractere == %c , nboccurence == %d, code == %s\n", alphabet[indice].caractere + PREMIER_CARACTERE, alphabet[indice].occurence, alphabet[indice].code);
+    qsort(alphabet, TAILLE_TABLEAU, sizeof(caractereOccurence), fonctionDeComparaison); //trie du tableau
 
 }
 
 
-/*
- 3- 7 Fonction pour creer l'arbre
-
-    :param -> un tableau (dans notre cas ce sera le tableau deja trie des caracteres et leur occurence)
-    :return -> un autre tableau qui contient les racines de notre arbre - suivant le principe de huffman 
-*/
-
-void construireArbre(){
+/*Construction de l'arbre de compression de huffman */
+void ArbreDeCompression(){
     int feuille = 0, racine = 0, k; 
 
     printf("\nDebut de la construction de l'arbre\n\n");
@@ -146,8 +110,9 @@ int main(int nbArgument, char *parametres[]){
         exit(1);
     }
 
+
     initialisationTableauAlphabet();
-    compterOccurenceMot(parametres[1]);
-    construireArbre();    
+    compterOccurence(parametres[1]);
+    ArbreDeCompression();    
     return 0;
 }
