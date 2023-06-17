341924d5b1bb5068c7434c3b2de8d085b99ae2b1
linabilou <biloapauline01@gmail.com>
2023-06-17 13:23:38 +0100

    huffman compression decompression version 1.0 OK

--- README.md
+++ README.md
@@ -1,14 +1,58 @@
 # Code de Compression et de decompression de Huffman
 
+
+# Description
+
+	Code de huffman compression et decompression ecrit en C.
+
+
 # Manuel d'utilisation 
+	
+	1- pour la compression 
+		1.1 compiler le code source : gcc huffman_compression.c
+		1.2 ./a.out chemin/vers/fichier/a/compressee
 
-gcc huffman_compression && ./a.out fichierACompresser //doit renvoyer le fichier comprenant : 
-Caractere	Occurence / Frequence	Code
+	2- pour la decompression
+		2.1 compiler le code source: gcc huffman_decompression.c
+		2.2 ./a.out chemin/vers/fichier/compresse chemin/vers/la/cle  
 
-# Compression 
+# PRINCIPE DE COMPRESSION # 
+
+# constante TAILLE_TABLEAU 
+    - C'est la taille de notre tableau de caractere. On part sur la base du code ASCII (127 caracteres)
+
+# constante PREMIER_CARACTERE
+    - Notre 1er caractere est le 32e caractere du code ASCII
+
+# int noeudParent[TAILLE_TABLEAU];
+    - Contient les racines obtenus a partir de la somme des occurences selon le principe de huffman
+
+# int nbTotalCaractere = 0; 
+    - Nombre total de caractere contenu dans le fichier a compresser
+
+# int nbCaractereUnique = 0; 
+    - Nombre total de caractere qui forment les mots du fichier a compresser
+
+# caractereOccurence alphabet[TAILLE_TABLEAU]; 
+    - Le tableau alphabet contient les caracteres ascii 32-126 d'ou la taille 95 (127-32)
+    - Il contient les caracteres ascii possibles et leur donne d'occurence
+
 Description detaille de chaque fonction
-...
-# la fonction compterOccurence(char *cheminVersFichier) 
+
+# La fonction initialisationTableauAlphabet()
+    - Permet d'initialiser le tableau de l'alphabet
+    - Lors de l'initialisation, les caracteres de chaque case correspondent aux indices du tableau et les occurences sont mis a 0 pour tout le monde
+    - Tout ceci pour nous faciliter la manipulation. 
+
+# La fonctionDeComparaison(const void *parametre1, const void *parametre2)
+    - Compare deux éléments 
+    - Va etre utilise dans qsort qui est une fonction deja definie en C et a une structure bien definie
+    - Elle prend en parametre deux pointeurs sur des constantes de n'importe quel type d'ou le void; et renvoie un entier
+    - Dans notre cas on fait en sorte qu'elle prenne des pointeurs sur le type qu'on a defini
+    - Renvoie
+
+
+# La fonction compterOccurence(char *cheminVersFichier) 
     - prend en parametre le fichier a compresser
     - calcule le nombre d'occurence de chaque caractere contenu dans le fichier a compresser
     - trie le tableau alphabet[TAILLE_TABLEAU] dans l'ordre croissant des occurences
@@ -16,38 +60,15 @@ Description detaille de chaque fonction
         - elle est definie comme suit:  void qsort(void *base, size_t nitems, size_t size, int (*compar)(const void *, const void*)) 
     - ne retourne rien mais nous donne le tableau trie a la fin de la procedure
 
+# La fonction ArbreDeCompression()
+    - Permet de construire l'arbre binaire de compression selon le principe de huffman
 
-/* Le tableau alphabet contient les caracteres ascii 32-126 d'ou la taille 95 (127-32)
-- Il contient les caracteres ascii possibles et leur donne d'occurence
-*/
-caractereOccurence alphabet[TAILLE_TABLEAU]; 
+# La fonction void enregistrerFrequence()
+    - Permet d'ecrire les caracteres sous leur valeur numerique ascii et leur frequences ou occurences 
+    - On obtient un fichier nomme "cleDecompression" qui servira a la decompression
 
+# La fonction enregistrerFichierCompresser(char *cheminVersFichier)
+    - Permet d'enregistrer le fichier compresse
 
-
-/*
- 3- 7 Fonction pour creer l'arbre
-    :param -> un tableau (dans notre cas ce sera le tableau deja trie des caracteres et leur occurence)
-    :return -> un autre tableau qui contient les racines de notre arbre - suivant le principe de huffman 
-*/
-
-void ArbreDeCompression()
-
-
-*
-Cette fonction  compare deux éléments > Elle va etre utilise dans qsort qui est une fonction deja definie en C et a une structure bien definie
-- Elle prend en parametre deux pointeurs sur des constantes de n'importe quel type d'ou le void; et renvoie un entier
-- Dans notre cas on fait en sorte qu'elle prenne des pointeurs sur le type qu'on a defini
-*/
-int fonctionDeComparaison (const void *parametre1, const void *parametre2)
-
-
-
-*
-cette fonction permet d'initialiser le tableau de l'alphabet
-- Lors de l'initialisation, les caracteres de chaque case correspondent aux indices du tableau et les occurences sont mis a 0 pour tout le monde
-Tout ceci pour nous faciliter la manipulation. 
-*/
-void initialisationTableauAlphabet()
-
-
-# Decompression 
+# PRINCIPE DE DECOMPRESSION # 
+...
--- huffman_compression.c
+++ huffman_compression.c
@@ -2,8 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define TAILLE_TABLEAU 95                                                                                                               
-#define PREMIER_CARACTERE 32    
+#define TAILLE_TABLEAU 128  
 
 typedef struct caractereOccurence{
     char caractere;
@@ -16,6 +15,12 @@ caractereOccurence alphabet[TAILLE_TABLEAU];
 
 /* Le tableau noeudParent contient les racines obtenus a partir de la somme des occurences selon le principe de huffman */
 int noeudParent[TAILLE_TABLEAU]; 
+
+int nbTotalCaractere = 0; //nombre total de caractere contenu dans le fichier a compresser
+
+int nbCaractereUnique = 0; //nombre total de caractere qui forment les mots du fichier a compresser
+
+char *texte;
 
 /*Initialisation de notre alphabet de caractere */
 void initialisationTableauAlphabet(){
@@ -38,70 +43,140 @@ int fonctionDeComparaison (const void *parametre1, const void *parametre2){
 
 /*Procedure pour compter le nombre d'occurence et trie le tableau */
 void compterOccurence(char *cheminVersFichier){
-    char caractereLu;
-    FILE *fichierACompressser;
-    if ((fichierACompressser = fopen(cheminVersFichier,"r")) == NULL){
-      fprintf(stderr, "\nErreur: Impossible de lire le fichier %s\n",cheminVersFichier);
-      exit(1);
-    }
-    while ((caractereLu = fgetc(fichierACompressser)) != EOF){
-        int indice = caractereLu - PREMIER_CARACTERE; //indice represente l'indice du caractere lu dans le tableau
-        if(indice >= 0 && indice < TAILLE_TABLEAU)
-            alphabet[indice].occurence++;
-            
-    }
-    fclose(fichierACompressser);
+	char caractereLu;
+    	FILE *fichierACompressser;
+    	if ((fichierACompressser = fopen(cheminVersFichier,"r")) == NULL){
+      		fprintf(stderr, "\nErreur: Impossible de lire le fichier %s\n",cheminVersFichier);
+      		exit(1);
+    	}
+    	while ((caractereLu = fgetc(fichierACompressser)) != EOF){
+		{
+			if(alphabet[caractereLu].occurence == 0)
+				nbCaractereUnique++;
 
-    qsort(alphabet, TAILLE_TABLEAU, sizeof(caractereOccurence), fonctionDeComparaison); //trie du tableau
-
+            		alphabet[caractereLu].occurence++;         
+			nbTotalCaractere++;
+		}
+    	}
+    	fclose(fichierACompressser);
+    	
+    	qsort(alphabet, TAILLE_TABLEAU, sizeof(caractereOccurence), fonctionDeComparaison);
 }
 
 
 /*Construction de l'arbre de compression de huffman */
 void ArbreDeCompression(){
-    int feuille = 0, racine = 0, k; 
+    /*
+        - feuille est l'indice pour parcourir le tableau alphabet comprenant tous les noeuds enfants (caractere de la chaine)
+        - racine est l'indice pour parcourir le tableau des racines de l'arbre   
+        - k = feuille + 2  
+    */
+   int feuille = 0, racine = 0, k; 
 
-    printf("\nDebut de la construction de l'arbre\n\n");
-    printf("             .........             \n\n");
-    while(feuille < TAILLE_TABLEAU && alphabet[feuille].occurence == 0)
-        feuille++;
+    	printf("\nDebut de la construction de l'arbre\n\n");
+    	printf("             .........             \n\n");
+    	while(feuille < TAILLE_TABLEAU && alphabet[feuille].occurence == 0)
+        	feuille++;
 
-    if(TAILLE_TABLEAU - feuille <= 1 )
-       racine = feuille;
-    else{
-        noeudParent[racine] = alphabet[feuille].occurence + alphabet[feuille + 1].occurence;
-        racine++;
-        for(k = feuille+2; k < TAILLE_TABLEAU; k++){
-            noeudParent[racine] = alphabet[k].occurence + noeudParent[racine -1];
-            racine++;
-        }
-    }
-    printf("\nFin de la construction de l'arbre\n\n");
+    	if(TAILLE_TABLEAU - feuille <= 1 )
+       		racine = feuille;
+    	else
+	{
+        	noeudParent[racine] = alphabet[feuille].occurence + alphabet[feuille + 1].occurence;
+        	racine++;
+        	for(k = feuille+2; k < TAILLE_TABLEAU; k++)
+		{
+            		noeudParent[racine] = alphabet[k].occurence + noeudParent[racine -1];
+            		racine++;
+        	}
+    	}
+    	printf("\nFin de la construction de l'arbre\n\n");
 
-    racine--;
-    feuille = --k;
-    printf("\nDebut de construction des codes\n\n");
-    char chaine[TAILLE_TABLEAU] = ""; //on se met au niveau de la racine pour commencer le parcours
-    while(racine >= 1){
-        strcpy(alphabet[feuille].code, chaine);
-        if(noeudParent[racine - 1] > alphabet[feuille].occurence){
-            strcat(alphabet[feuille].code, "0");
-            strcat(chaine, "1"); //chaine = chaine + 1
-        }else{
-            strcat(alphabet[feuille].code, "1");
-            strcat(chaine, "0");
-        }
-        feuille--;
-        racine--; 
-    }
+    	racine--;
+    	feuille = --k;
+    	printf("\nDebut de construction des codes\n\n");
+    	char chaine[TAILLE_TABLEAU] = ""; //on se met au niveau de la racine pour commencer le parcours
+    	while(racine >= 1)
+	{
+        	strcpy(alphabet[feuille].code, chaine);
+        	if(noeudParent[racine - 1] > alphabet[feuille].occurence)
+		{
+            		strcat(alphabet[feuille].code, "0");
+            		strcat(chaine, "1"); //chaine = chaine + 1
+        	}
+		else
+		{
+            		strcat(alphabet[feuille].code, "1");
+            		strcat(chaine, "0");
+        	}
+        	feuille--;
+        	racine--; 
+    	}
 
-    strcat(strcpy(alphabet[feuille].code, chaine), "1");
-    strcat(strcpy(alphabet[feuille - 1].code, chaine), "0");
+    	strcat(strcpy(alphabet[feuille].code, chaine), "1");
+    	strcat(strcpy(alphabet[feuille - 1].code, chaine), "0");
 
-    for(int indice = 0; indice < TAILLE_TABLEAU; indice++)
-       if(alphabet[indice].occurence != 0)
-           printf("id == %d, caractere == %c , nboccurence == %d, code == %s\n", indice, alphabet[indice].caractere + PREMIER_CARACTERE, alphabet[indice].occurence, alphabet[indice].code);
-    printf("\nFin de construction des codes\n\n");
+    	for(int indice = 0; indice < TAILLE_TABLEAU; indice++)
+       		if(alphabet[indice].occurence != 0)
+           		printf("id == %d, caractere == %c , nboccurence == %d, code == %s\n", indice, alphabet[indice].caractere, alphabet[indice].occurence, alphabet[indice].code);
+
+    	printf("\nFin de construction des codes\n\n");
+}
+
+
+/* Ecrire les frequences dans un fichier */
+void enregistrerFrequence()
+{
+	FILE *fichierArbre = fopen("cleDecompression", "w");
+	fprintf(fichierArbre, "%d\n", nbCaractereUnique);
+	for(int indice = 0; indice < TAILLE_TABLEAU; indice++)
+	{
+		if(alphabet[indice].occurence != 0)
+		{
+			char caractere = alphabet[indice].caractere;
+			int occurence = alphabet[indice].occurence;
+		}
+	}
+	fclose(fichierArbre);
+}
+
+/* Sortir le fichier compresse */
+void enregistrerFichierCompresser(char *cheminVersFichier)
+{
+	char caractereLu;
+    	FILE *fichierACompressser;
+    	if ((fichierACompressser = fopen(cheminVersFichier,"r")) == NULL){
+      		fprintf(stderr, "\nErreur: Impossible de lire le fichier %s\n",cheminVersFichier);
+ 	     	exit(1);
+    	}
+	texte = malloc(sizeof(char) * nbTotalCaractere);
+	unsigned int car = 0;
+    	while ((caractereLu = fgetc(fichierACompressser)) != EOF)
+		texte[car++] = caractereLu;
+
+    	fclose(fichierACompressser);
+
+
+	FILE *fichierCompresse = fopen("fichierCompresse", "w");
+
+
+	for(car = 0; car < nbTotalCaractere; car++)
+	{
+		int indice = texte[car];
+		int id = TAILLE_TABLEAU;
+		while(alphabet[id].caractere != indice)
+			id--;
+		fprintf(fichierCompresse, "%s", alphabet[id].code);
+	}
+
+	
+
+	fclose(fichierCompresse);
+	free(texte);
+	printf("\n		Compression terminee avec SUCESS		\n");
+	printf("\n\nLes fichier suivant ont ete genere:\n");
+	printf("		1- fichierCompresse : contient le texte compresse\n");
+	printf("		2- cleDecompression : la cle qui va permettre de decompresser\n\n");
 }
 
 int main(int nbArgument, char *parametres[]){
@@ -110,9 +185,11 @@ int main(int nbArgument, char *parametres[]){
         exit(1);
     }
 
-
     initialisationTableauAlphabet();
     compterOccurence(parametres[1]);
-    ArbreDeCompression();    
+    ArbreDeCompression();   
+    enregistrerFrequence();
+	enregistrerFichierCompresser(parametres[1]);
+
     return 0;
 }
